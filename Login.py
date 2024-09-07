51f0eba13838070d7be30e18df0367aa8668ab00
Auzek2002 <azaanukhan@gmail.com>
2024-09-07 13:30:04 +0500

    Frontend done, need to connect cloud

---
+++ Login.py
@@ -0,0 +1,98 @@
+import json
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import requests
+
+def login(username, password):
+    # print(f'{username}, {password}')
+    headers = {
+    'Content-Type': 'application/json'}
+    url='http://localhost:5000/user/login'
+    body={'userID':username, 'password':password}
+    body_json=json.dumps(body)
+    # print(body_json)
+    response = requests.post(url=url,headers=headers, data=body_json)
+    # print('Status Code:', response.status_code)
+    # print('Response Body:', response.json())
+    return response
+    # pass
+
+def signup(username, password):
+    headers = {
+    'Content-Type': 'application/json'}
+    url='http://localhost:5000/user/signup'
+    body={'userID':username, 'password':password}
+    body_json=json.dumps(body)
+    # print(body_json)
+    response = requests.post(url=url,headers=headers, data=body_json)
+    # print('Status Code:', response.status_code)
+    # print('Response Body:', response.json())
+    return response
+    pass
+
+st.markdown("""
+    <style>
+        section[data-testid="stSidebar"][aria-expanded="true"]{
+            display: none;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+# Define the valid username and password
+# valid_username = "user"
+# valid_password = "password"
+
+# Page title
+st.title("Login/Sign Up Page")
+
+# Display the login form
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+# Check if the user clicked the "Login" button
+if st.button("Login"):
+    # response = login(username,password)
+
+    # if(response.status_code==200):#successfull login
+    # # if username == valid_username and password == valid_password:
+    #     response=response.json()
+    #     st.session_state.token = response['authorization']
+    #     st.success("Login successful!")
+    #     st.markdown("""
+    # <style>
+    #     section[data-testid="stSidebar"][aria-expanded="true"]{
+    #         display: block;
+    #     }
+    # </style>
+    # """, unsafe_allow_html=True)
+    if username == 'Azaan' and password == '123':
+         st.success("Login successful!")
+         st.markdown("""
+    <style>
+        section[data-testid="stSidebar"][aria-expanded="true"]{
+            display: block;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+
+    else:
+        st.error("Invalid username or password. Please try again.")
+
+# Display a sign-up form
+st.write("Don't have an account? Sign up below.")
+new_username = st.text_input("New Username")
+new_password = st.text_input("New Password", type="password")
+confirm_password = st.text_input("Confirm Password", type="password")
+
+# Check if the user clicked the "Sign Up" button
+if st.button("Sign Up"):
+    if new_password == confirm_password:
+        response = signup(new_username,new_password)
+        if(response.status_code==200):
+            st.success("Account created successfully! Please login.")
+        else:    
+            st.error("User Already Exists. Please login or chose another username")
+    else:
+        st.error("Passwords do not match. Please try again.")
+
+
---
+++ Pages/1_Chat.py
@@ -0,0 +1,62 @@
+import json
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import requests
+import math
+import numpy as np
+import google.generativeai as genai
+import os
+
+
+
+genai.configure(api_key="AIzaSyDqXlh83m8LivN-Nt2FeVVNUdTpvMqSwfk")     #Add Your API Key here
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+def genAnswer(prompt):
+    response = model.generate_content(prompt)
+    output_text = response.text
+    return output_text
+
+#This is to create the storage for the messages:
+
+st.markdown(
+    """
+    <style>
+    .sticky-title {
+        position: -webkit-sticky;
+        position: sticky;
+        top: 0;
+        background-color: white;
+        padding: 10px;
+        z-index: 1000;
+        border-bottom: 2px solid #f0f0f0;
+    }
+    </style>
+    <div class="sticky-title">
+        <h1>Cloud-Based ChatBot</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#Display the chat history:
+# st.write(st.session_state.messages)
+for messages in st.session_state.messages:
+    with st.chat_message(messages.get("role")):
+        st.write(messages.get("content"))
+
+prompt = st.chat_input("Ask anything")
+if prompt:
+    st.session_state.messages.append({"role" : "user", "content" : prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+        answer = genAnswer(prompt)
+
+    st.session_state.messages.append({"role" : "assistant", "content" : answer})
+    with st.chat_message("assistant"):
+        st.write(answer)
+
+
